86f922e43d52bb4c52b1597d4637f7eb3fa9b12a
Fabiandy333 <fabiandy2020@gmail.com>
2025-03-16 23:00:41 -0500

    login 16/03/2025

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,9 +1,43 @@
-import "./Login.css"
+import "./Login.css";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
   return (
-    <h1>Inicio de sesión</h1>
-  )
-}
+    <div className="login-container">
+      <div className="hero-content">
+        <h1 className="main-title">Explora los órganos en 3D.</h1>
+        <p className="subtitle">Si ya tienes una cuenta ¡empieza ahora!</p>
+      </div>
 
-export default Login
+      <div className="login-section">
+        <h2 className="welcome-title">Bienvenido de nuevo</h2>
+        
+        <div className="input-group">
+          <input 
+            type="email" 
+            placeholder="Ingresar el correo"
+            className="login-input"
+          />
+          <input 
+            type="password" 
+            placeholder="*********"
+            className="login-input"
+          />
+        </div>
+        
+        <NavLink to="/recuperar-contrasena" className="forgot-password">
+          ¿Olvidaste tu contraseña?
+        </NavLink>
+        
+        <NavLink 
+          to="/pagina-inicio" 
+          className="auth-link login"
+        >
+          Iniciar sesión
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
